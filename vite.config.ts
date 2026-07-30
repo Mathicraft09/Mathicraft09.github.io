7c371ab2b0472d7aca7b559ed49f3cbe8ab5fa65
Mathicraft09 <abbetmathieu.am@gmail.com>
2026-07-30 16:15:48 +0200

    Deploy React app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/monSite/', // <-- Remplacez "monSite" par le nom exact de votre dépôt GitHub
+  base: '/Mathicraft09.github.io/', // <-- Remplacez "monSite" par le nom exact de votre dépôt GitHub
   plugins: [react()],
 })
